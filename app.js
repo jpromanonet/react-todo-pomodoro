d64007572976ebe2750cb6571cac31f8c972a839
Tony Spiro <tspiro@tonyspiro.com>
2018-01-24 14:46:19 -0600

    try static again

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-app.set('port', process.env.PORT || 3000)
-app.use(express.static('static'))
-app.listen(app.get('port') || 3000, () => {
-  console.info('==> 🌎  Go to http://localhost:%s', app.get('port'))
-})
