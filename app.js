9eb18f5cc5ce96060ca508d6f26f12269791ad2e
Tony Spiro <tspiro@tonyspiro.com>
2018-01-24 14:28:25 -0600

    rm redirect

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var express = require('express')
 var app = express()
 app.set('port', process.env.PORT || 3000)
 app.use(express.static('static'))
-app.get('*', (req, res) => {
-  res.redirect('/404')
-})
 app.listen(app.get('port') || 3000, () => {
   console.info('==> 🌎  Go to http://localhost:%s', app.get('port'))
 })
