dc0125941616e7ab5b3ed88018c6606a71ba193e
Jazib Sawar <jazibsawar@gmail.com>
2018-02-07 21:48:32 +0500

    Cosmetic changes and build process

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,14 +3,15 @@ import isDev from '../isDev';
 const config = {
 	bucket: {
 		slug: 'pomodorotimer',
-		read_key: '',
-		write_key: '',
+		read_key: '5K5zvM4TanZ5wX6LLu7oqPxGoiJ6O58aobuTOYf7kzPTN9kaDi',
+		write_key: 'ZoqKPjS8SJcM2Ta1LXkCk5rYSCI4r0uSbL8lBxhgxkBSwit8T1',
 	}
 };
 
 if (!isDev()) {
-	config.bucket.read_key = process.env.READ_KEY;
-	config.bucket.write_key = process.env.WRITE_KEY;
+	config.bucket.slug = process.env.COSMIC_BUCKET;
+	config.bucket.read_key = process.env.COSMIC_READ_KEY;
+	config.bucket.write_key = process.env.COSMIC_WRITE_KEY;
 }
 
 export default config;
